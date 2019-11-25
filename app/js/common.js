d14a616348c4ab81777480e4477a9bc30b7afb87
Anton Beren Ivanov <anton@svoj5.ru>
2019-11-25 14:15:48 +0400

    add 2gis map

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,23 @@
 $(function() {
 
 	// Custom JS
+	var map;
+
+            DG.then(function () {
+                map = DG.map('map', {
+                    center: [42.871877, 74.586491],
+                    zoom: 13
+                });
+
+				mapicon = DG.icon({
+					iconUrl: '../img/marker.svg', /* Иконка маркера */
+					iconAnchor: [42.871877, 74.586491], 
+					popupAnchor: [42.871877, 74.586491],
+					className: 'map-icon',
+					iconSize: [64, 64] /* Размер иконки */
+				});          
+				DG.marker([42.871877, 74.586491], {icon: mapicon}).addTo(map).bindPopup('<div class="map-popup"><h2>FamsTour</h2><br/>Авто и мото туры<br/>по лесным и горным маршрутам</div>');     /* Координаты маркера и текст попапа */
+            });
+
 
 });
