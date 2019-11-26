9aeb521c5858ee135302c941ea187940d4f7d9c0
Anton Beren Ivanov <anton@svoj5.ru>
2019-11-26 15:21:00 +0400

    castom fancybox

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,4 @@
-; (function ($) {
+(function ($) {
     "use strict";
 
     $(document).ready(function () {
@@ -860,9 +860,47 @@
 
 })(jQuery);
 
-(function($) {
-    $('[data-fancybox="gallery"]').fancybox({
-        // Options will go here
-    });
 
-      })(jQuery_3_2_1);
+
+(function($) {
+
+    $('[data-fancybox="gallery"]').fancybox({
+        buttons : [ 
+            'zoom',
+            'slideShow',
+            // 'share',
+            // 'fullScreen',
+            // 'download',
+            "thumbs",
+            'close'
+        ],
+        loop: true,
+        gutter : 10,
+        keyboard: true,
+        arrows: true,
+        infobar: true,
+        smallBtn: "auto",
+        toolbar: true,
+        protect: true,
+        modal: false,
+        idleTime: 3,
+        animationEffect: "zoom",
+        animationDuration: 600,
+        transitionEffect: "fade",
+        transitionDuration: 400,
+        slideClass: "myClass",
+        baseClass: "myclass",
+        slideShow: {
+            autoStart: false,
+            speed: 1000
+        },
+        youtube : {
+        controls : 0,
+        showinfo : 0
+        },
+        thumbs : {
+            autoStart : false
+        }
+        });
+})(jQuery_3_4_1);
+
